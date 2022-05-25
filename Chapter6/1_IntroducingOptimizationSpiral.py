7691c81dd63500341e828b5889ba5daeeacbff71
Jakub <jakub.kornecki1@gmail.com>
2022-05-25 19:34:48 +0200

    Chapter6 Done

--- Chapter6/1_IntroducingOptimizationSpiral.py
+++ Chapter6/1_IntroducingOptimizationSpiral.py
@@ -1,6 +1,6 @@
 import matplotlib.pyplot as plt
 import nnfs
-from nnfs.datasets import vertical_data
+from nnfs.datasets import spiral_data
 import numpy as np
 
 class Layer_Dense:
@@ -88,7 +88,7 @@ class Loss_CategoricalCrossentropy(Loss):
 nnfs.init()
 
 # Create a dataset
-X, y = vertical_data(samples=100, classes=3)
+X, y = spiral_data(samples=100, classes=3)
 # plt.scatter(X[:, 0], X[:, 1], c=y, s=40, cmap='brg')
 # plt.show()
 
